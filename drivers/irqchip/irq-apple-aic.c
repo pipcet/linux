051e9f7894d4ddac693434a12f32e85816412622
Pip Cet <pipcet@gmail.com>
2021-08-11 07:44:27 +0000

    minor

--- drivers/irqchip/irq-apple-aic.c
+++ drivers/irqchip/irq-apple-aic.c
@@ -162,7 +162,7 @@ static void __exception_irq_entry aic_handle_irq(struct pt_regs *regs)
 		irq = FIELD_GET(AIC_EVENT_NUM, event);
 
 		if (type == AIC_EVENT_TYPE_HW)
-			handle_domain_irq(aic_irqc->hw_domain, irq, regs);
+			handle_domain_irq(ic->hw_domain, irq, regs);
 		else if (type == AIC_EVENT_TYPE_IPI && irq == 1)
 			aic_handle_ipi(regs, 0);
 		else if (event != 0)
