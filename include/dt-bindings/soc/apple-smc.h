530358e0bc7219687b9a49ea57cd5a987d5beb64
Pip Cet <pipcet@gmail.com>
2021-08-16 18:54:00 +0000

    new files

---
+++ include/dt-bindings/soc/apple-smc.h
@@ -0,0 +1,4 @@
+#define SMC_IRQ_BATT 0 /* power button, lid switch */
+#define SMC_IRQ_HID  1 /* battery charging/discharging/fully charged */
+#define SMC_IRQ_REST 2 /* unknown notification */
+#define NUM_SMC_IRQ  3
