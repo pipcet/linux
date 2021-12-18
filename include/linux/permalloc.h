00971b4814cc2fc752aa3ee8d542e615d1daa146
Pip Cet <pipcet@gmail.com>
2021-12-18 16:35:41 +0000

    include/linux/permalloc.h: support debugging string

--- include/linux/permalloc.h
+++ include/linux/permalloc.h
@@ -7,10 +7,12 @@ struct permalloc_entry {
 	struct list_head list;
 	struct device *dev;
 	const char *str;
+	const char *str2;
 };
 
 extern int permalloc_bool(struct device *dev, const char *name);
 extern int permalloc_memory(struct device *dev, void *memory, size_t size);
 extern int permalloc_spin_table(phys_addr_t spin_table_pa);
 extern int permalloc_spin_code(phys_addr_t spin_code_pa);
+extern void permalloc_init(void);
 #endif
